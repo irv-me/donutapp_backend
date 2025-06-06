c1072e55da68cdfc1dbcfd0d0c20fb2275ab1260
irv-me <tareastareas89@gmail.com>
2025-06-06 13:21:26 -0600

    first commit

---
+++ src/main/java/com/tecdesoftware/demo/DonutappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tecdesoftware.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonutappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonutappBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tecdesoftware/demo/DonutappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecdesoftware.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonutappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
